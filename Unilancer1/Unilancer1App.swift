2edfccee114461f7447dda78f93115ba87ebc73f
Reemdeva <alharthira93@gmail.comalharthira93@gmail.com>
2022-06-06 11:43:18 +0300

    Initial Commit

---
+++ Unilancer1/Unilancer1App.swift
@@ -0,0 +1,17 @@
+//
+//  Unilancer1App.swift
+//  Unilancer1
+//
+//  Created by Reem Alharthi on 07/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Unilancer1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
